fb229d7247a302f69146b2c8e23bb67f7ca771ac
George Brian <georgebriang@icloud.com>
2023-06-03 22:03:20 -0500

    last update

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import './assets/css/base/base.css';
+import './assets/css/componentes/card.css'
+import Home from "./pages/Home"
+import Sobre from "./pages/Sobre"
+import Page404 from './pages/Pages404';
+import Header from './Components/Header';
+import Post from './pages/Post';
+import Categoria from './pages/Categoria';
+
+
+
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
+function App() {
+
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/Sobre" element={<Sobre />}/>
+        <Route path="/Posts/:id" element={<Post />}/>
+        <Route path="/categoria/:id/*" element={<Categoria />} />
+        <Route path="*" element={<Page404 />} />
+
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
